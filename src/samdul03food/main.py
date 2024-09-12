db9ae53cbdf6a35002b3358e2a553877b1f521e4
DONGUK777 <kimdongyork@gmail.com>
2024-09-12 19:23:34 +0900

    edit main.py

--- src/samdul03food/main.py
+++ src/samdul03food/main.py
@@ -17,19 +17,20 @@ def get_path():
     dir = os.path.dirname(file_dir)
     return dir
 
+# 서버가 시작될 때 헤더가 없는 파일일 경우, 헤더 추가
 if not os.path.exists(csv_file_path):
-    with open(csv_file_path, mode='w', newline='', encoding='utf-8') as file:
-        writer = csv.writer(file)
-        writer.writerow(["food", "time"])  # 헤더 추가
+    df = pd.DataFrame(columns=["food", "time"])
+    df.to_csv(csv_file_path, index=False, encoding='utf-8')  # 헤더와 빈 데이터로 파일 생성
 
 @app.get("/food")
 def food(name: str):
     # 현재 시간을 저장
-    time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    
-    # 음식 이름과 시간을 csv로 저장 _> /code/data/food.csv
-    with open(csv_file_path, mode='a', newline='', encoding='utf-8') as file:
-        writer = csv.writer(file)
-        writer.writerow([name, current_time])  # 데이터 추가
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-    return {"food":name, "time": time}
+    # 음식 이름과 시간을 DataFrame으로 변환
+    df = pd.DataFrame([[name, current_time]], columns=["food", "time"])
+
+    # DataFrame을 CSV 파일에 추가
+    df.to_csv(csv_file_path, mode='a', header=False, index=False, encoding='utf-8')
+
+    return {"food": name, "time": current_time}
