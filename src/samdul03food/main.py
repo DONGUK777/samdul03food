d7e5d4cdc3dd5c5e33bc25d3b81574799e830c5a
DONGUK777 <kimdongyork@gmail.com>
2024-09-12 19:35:06 +0900

    edit main.py

--- src/samdul03food/main.py
+++ src/samdul03food/main.py
@@ -1,36 +1,27 @@
 from typing import Union
-import pickle
+import pandas as pd
+from datetime import datetime
 from fastapi import FastAPI
-import datetime
+import os
 
 app = FastAPI()
 
 # CSV 파일이 저장될 경로
 csv_file_path = "/code/data/food.csv"
 
-@app.get("/")
-def read_root():
-    return {"Hello": "n02"}
-
-def get_path():
-    file_dir = __file__
-    dir = os.path.dirname(file_dir)
-    return dir
-
 # 서버가 시작될 때 헤더가 없는 파일일 경우, 헤더 추가
 if not os.path.exists(csv_file_path):
     df = pd.DataFrame(columns=["food", "time"])
     df.to_csv(csv_file_path, index=False, encoding='utf-8')  # 헤더와 빈 데이터로 파일 생성
 
+@app.get("/")
+def read_root():
+    return {"Hello": "n02"}
+
 @app.get("/food")
 def food(name: str):
     # 현재 시간을 저장
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    
-    if not os.path.exists(csv_file_path):
-        df = pd.DataFrame(columns=["food", "time"])
-    else:
-        df = pd.read_csv(csv_file_path)
 
     # 음식 이름과 시간을 DataFrame으로 변환
     df = pd.DataFrame([[name, current_time]], columns=["food", "time"])
