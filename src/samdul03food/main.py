472a73ef53275161e3062edce69b2e3ac47214a4
DONGUK777 <kimdongyork@gmail.com>
2024-09-12 19:06:37 +0900

    add main.py

--- src/samdul03food/main.py
+++ src/samdul03food/main.py
@@ -4,12 +4,21 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# CSV 파일이 저장될 경로
+csv_file_path = "/code/data/food.csv"
+
 @app.get("/")
 def read_root():
     return {"Hello": "n02"}
 
 @app.get("/food")
 def food(name: str):
-    # 시간을 구함
+    # 현재 시간을 저장
+    time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    
     # 음식 이름과 시간을 csv로 저장 _> /code/data/food.csv
-    return {"food":name, "time": "2024-09-15 11:12:13"}
+    with open(csv_file_path, mode='a', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow([name, current_time])  # 데이터 추가
+
+    return {"food":name, "time": time}
