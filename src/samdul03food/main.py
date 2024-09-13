40a97e68c932d9e16846f5f72f2fd1f9bbc1c273
DONGUK777 <kimdongyork@gmail.com>
2024-09-13 16:34:20 +0900

    fix main

--- src/samdul03food/main.py
+++ src/samdul03food/main.py
@@ -11,6 +11,7 @@ app = FastAPI()
 origins = [
     "http://localhost:8899",
     "http://127.0.0.1:8899",
+    "http://samdul03food-83d92.web.app",
     "http://127.0.0.1:8877",
     # 다른 도메인도 필요 시 추가 가능
 ]
